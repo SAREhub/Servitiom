bbbc1384917a32fb0cb6435fbd77a3583edc4894
a.wasiak@sare.pl <a.wasiak@sare.pl>
2018-09-14 14:28:22 +0200

    [WIP] changed bin/test gearmand from artefactual/gearmand

--- bin/test/gearmand/deploy.sh
+++ bin/test/gearmand/deploy.sh
@@ -6,24 +6,13 @@ source ./bin/test/.env
 set +a
 
 docker service create \
-    --name "gearmand"  \
-    --network "$NETWORK" \
+    --name "${GEARMAND_SERVICE}"  \
+    --network "${NETWORK}" \
     --label "${TESTENV_LABEL}" \
     --detach=true \
     --with-registry-auth \
-    "localhost:5000/gearman:test" \
-        --listen=0.0.0.0
-        --port=4730
-        --log-file=stderr \
-        --verbose=INFO \
-        --queue-type=MySQL \
-        --mysql-host=${DATABASE_HOST} \
-        --mysql-port=${DATABASE_PORT} \
-        --mysql-user=${DATABASE_USER} \
-        --mysql-password=test \
-        --mysql-db=gearmand \
-        --mysql-table=gearmand \
-    #&>/dev/null
-
+    --env QUEUE_TYPE="${GEARMAND_QUEUE_TYPE}" \
+    "${GEARMAND_SERVICE_IMAGE}" \
+    &>/dev/null
 
 dockerutil::print_success "created service: ${DATABASE_SERVICE}"
