d83ceb91031a88511f818947b3e7b15f1c106990
Mararok <mararok@gmail.com>
2018-09-17 07:08:22 +0200

    [WIP] added missing secret on prod scripts

--- bin/prod/gearmand/deploy.sh
+++ bin/prod/gearmand/deploy.sh
@@ -9,9 +9,9 @@ docker service create \
     --name "gearmand"  \
     --network "$NETWORK" \
     --label "${TESTENV_LABEL}" \
-    --secret "$GEARMAND_MYSQL_PASSWORD_SECRET" \
     --detach=true \
     --with-registry-auth \
+    --secret "$GEARMAND_MYSQL_PASSWORD_SECRET" \
     --env QUEUE_TYPE=$GEARMAND_QUEUE_TYPE \
     --env MYSQL_HOST=$GEARMAND_MYSQL_HOST \
     --env MYSQL_PORT=$GEARMAND_MYSQL_PORT \
