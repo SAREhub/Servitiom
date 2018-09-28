fb117dd90aa3b144bac4b0ff3480fb78f49d0ada
Mararok <mararok@gmail.com>
2018-09-28 18:51:25 +0200

    added ServiceJobTemplate

---
+++ src/SAREhub/Servitiom/Entity/Service/ServiceJobTemplate.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace SAREhub\Servitiom\Entity\Service\Job;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\UniqueConstraint;
+use SAREhub\Servitiom\Entity\Service\ServiceVersion;
+
+/**
+ * @Entity
+ * @Table(name="service_job_definitions"
+ *        uniqueConstraints={@UniqueConstraint(name="unique_job_template",columns={"name", "scopeType", "serviceVersion"})}
+ * )
+ **/
+class ServiceJobTemplate
+{
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Column(type="string", length=255)
+     * @var string
+     */
+    private $name;
+    private $scopeType;
+
+    /**
+     * @ManyToOne(targetEntity="ServiceVersion")
+     * @JoinColumn(onDelete="CASCADE")
+     * @var ServiceVersion
+     */
+    private $serviceVersion;
+
+    /**
+     * @Column(type="integer", options={"unsigned": true})
+     * @var int
+     */
+    private $createdAt;
+
+    /**
+     * @Column(type="text", length=65535)
+     * @var string
+     */
+    private $template;
+}
